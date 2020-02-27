75278574c090168d14c8d7c30f725ffac5645cc9
riley-jones <58481501+riley-jones@users.noreply.github.com>
2020-02-27 12:06:10 -0600

    Update Js.js

--- Image Letter/Js.js
+++ Image Letter/Js.js
@@ -1,31 +1,21 @@
 function append (message) {
     var alphabet = "abcdefghijklmnopqrstuvwxyz";
     
-    
     for(var i=0; i < message.length; i++) {
 
         var lett = message.charAt(i);
+             
         var currLetterIndex = alphabet.indexOf(lett);
 
         var imgName = "L_" + currLetterIndex + ".png";
 
-        var img = new Image();
-        var div = document.getElementById('imgName');
- 
- 
-img.onload = function() {
- 
-  div.innerHTML += '<img src="'+img.src+'" />'; 
- 
-};
- 
- 
-img.src = 'imgName';
+        $("#final").append("<img src='Images/"+imgName+"'>");
+
+        var message = $('#ui').val();
 
         console.log(imgName);
-
 
     }   
 }
 
-append("car");
+append('message');
